df70c02c86fcd17978a5bd4bce4906183fcaef19
pzbilkic <99282789+pzbilkic@users.noreply.github.com>
2022-05-11 12:21:47 -0700

    initial commit

---
+++ Game.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+
+import javax.swing.plaf.ColorUIResource;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Game {
+
+
+    private Square player = new Square(10, 200, 30, 30, new Color(25,25,150));
+	private ArrayList<GameObject> objects = new ArrayList<>();
+
+    public Game(){
+        
+    }
+    
+
+
+	/**
+	 * This is called every time the Timer goes off.  Right now, it moves all 
+	 * the Objects and checks for collisions.  This is common in games with flying
+	 * Objects.  You can do more, though.  Like add items or move to new screens
+	 * or check to see if the turn is over or...
+	 */
+	public void updateGame() {
+		//System.out.println("Moving Objects!!");
+		moveObjects();
+		checkCollisions();
+
+	}
+    /**
+	 * Right now I am checking for collisions between GameObjects
+	 */
+	public void checkCollisions() {
+		
+	}
+
+	/**
+	 * get it...
+	 */
+	public void moveObjects() {
+		//player.move();
+	}
+
+
+	/**
+	 * Draws all the stuff in the game without changing them
+	 * No reason to change this unless you wanted a background
+	 * or something.
+	 * @param g
+	 */
+    public void drawTheGame(Graphics g) {
+		player.draw(g);
+		g.setColor(new ColorUIResource(100, 200, 10));
+		g.fillRect(0, 230, 500, 200);
+		for(GameObject go:this.objects) {
+			go.draw(g);
+		}
+
+	}
+		
+	
+	/** called when the left arrow is pressed.  Probably move something
+	 *  to the left, or turn left or... */
+	public void ltHit(ActionEvent e) {
+		System.out.println("Left!!");
+	}
+	/** called when the left arrow is released.  You don't have to do anything
+	 * here, but you can if your game uses this event*/
+    public void leftReleased(ActionEvent e) {
+        System.out.println("Released Left!!");
+    }
+	/** called when the right arrow is pressed.  Probably move something
+	 *  to the right, or turn right or... */
+	public void rtHit(ActionEvent e) {
+        
+		System.out.println("Right!");
+	}
+	/** called when the right arrow is released.  You don't have to do anything
+	 * here, but you can if your game uses this event*/
+    public void rightReleased(ActionEvent e) {
+		System.out.println("Released &&&&  Right!!");
+    }
+	/** called when the up arrow is pressed.  You don't have to do anything
+	 * here, but you can if your game uses this event*/
+    public  void upHit(ActionEvent e) {
+		System.out.println("Up!!");
+		player.jump();
+		
+
+
+	}
+	/** called when the up arrow is released.  You don't have to do anything
+	 * here, but you can if your game uses this event*/
+	public void upReleased(ActionEvent e) {
+		System.out.println("Released ^^^^  Up!!");
+		
+    }
+
+	
+
+    
+
+}
---
+++ GameObject.java
@@ -0,0 +1,98 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Generic GameObject.  This has all the BASIC attributes and behaviors that 
+ * ALL game objects should have.  Many of these can be overridden in subclasses.
+ * Those behaviors and attributes include:
+ * 
+ * Location and size (using a Rectangle for that)
+ * Color (used for testing but not used in the final version)
+ * speed in the x and speed in the y direction
+ * The GameObject has the ability to move, detect collisions with 
+ * another GameObject and the ability to draw itself on a Graphics
+ * 
+ * @author RHanson
+ *
+ */
+public class GameObject {
+	
+	/** rect has info about location and dimension of this game object*/
+	private Rectangle rect;
+	/** Color of this object */
+	private Color color;
+	/** dx is how far this object moves this Rectangle each time I move
+	 *  dy is how far this object moves the Rectangle each time I move
+	 *  If dy or dx change between moves, it will look like this object is 
+	 *  accelerating or decelerating in that direction.
+	 */
+	private double dx, dy;
+	
+	//Method for square intialization
+	public GameObject(int x, int y, int w, int h, Color c) {
+		this(x,y,w,h,c,0,0);
+		this.rect = new Rectangle(x,y,w,h);
+		
+	}
+	public GameObject(int x, int y, int w, int h, Color c, int dx, int dy) {
+		rect = new Rectangle(x,y,w,h);
+		color = c;
+		this.dx = dx;
+		this.dy = dy;
+	}
+	public double getDx() {
+		return dx;
+	}
+	public void setDx(double dx) {
+		this.dx = dx;
+	}
+	
+	public double getDy() {
+		return dy;
+	}
+	public void setDy(double dy) {
+		this.dy = dy;
+	}
+	public void move() {
+		moveX();
+		moveY();
+	}
+	public void jump() {
+		this.dy = -10;
+		for(int i = (int)this.dy; i < 0; i++){
+			moveY();
+			dy++;
+			System.out.println(dy);
+			/*
+			try{Thread.sleep(100);}
+			catch(InterruptedException ex){
+    			Thread.currentThread().interrupt();}
+				*/
+		}
+
+		System.out.println("jumped up!");
+	}
+	public void moveY() {
+		rect.setLocation(rect.x+0, (int) (rect.y+dy));
+	}
+	public void moveX() {
+		rect.setLocation((int) (rect.x+dx), rect.y+0);
+	}
+	public Rectangle getRect() {
+		return rect;
+	}
+  
+	/** Pretty basic right now, but can make this way better!*/
+	public void draw(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(color);
+		g2.fill(rect);
+	}
+	
+	public boolean collidedWith(GameObject go) {
+		return this.rect.intersects(go.rect);
+	}
+}
