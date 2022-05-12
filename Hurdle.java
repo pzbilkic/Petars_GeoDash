6cd643f2f1d4f6e813f899055971f9bdc4174c03
wgreen13 <william.e.j.green@gmail.com>
2022-05-12 09:39:09 -0700

    hurdle

---
+++ Hurdle.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Hurdle extends GameObject {
+
+    public Hurdle(int x, int y, int w, int h, Color c, int dx, int dy) {
+        super(x, y, w, h, c, dx, dy);
+    }
+
+}
